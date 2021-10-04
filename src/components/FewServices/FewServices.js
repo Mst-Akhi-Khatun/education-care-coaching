406cd2b798de7fb8a9fad57a5f527c0efcd8a59c
Mst-Akhi-Khatun <abiraajmi@gmail.com>
2021-10-04 19:45:47 +0600

    done

--- src/components/FewServices/FewServices.js
+++ src/components/FewServices/FewServices.js
@@ -7,7 +7,7 @@ const FewServices = (props) => {
         <div className="col">
             <div className="card h-100 d-flex align-items-center border border-1 border-primary rounded-3 p-3">
                 <img src={img} className="card-img-top" width="50px" height="170px" alt="..." />
-                <div className="card-body">
+                <div className="card-body text-center">
                     <h3 className="card-title">{name}</h3>
                     <h6 className="card-text">{description}</h6>
                     <h4 className="card-text">Price: ${price}</h4>
