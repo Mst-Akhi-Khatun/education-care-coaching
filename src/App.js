b2eb1da6390792bea5972477cf9b89d16211861a
Mst-Akhi-Khatun <abiraajmi@gmail.com>
2021-10-04 18:56:03 +0600

    done

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ function App() {
         <Header></Header>
         <Switch>
           <Route exact path="/">
-
+            <Home></Home>
           </Route>
           <Route path="/home">
             <Home></Home>
